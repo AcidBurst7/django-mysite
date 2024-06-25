a6bc7c68ce00dd2ff6a2545b4f5127cc40622b17
varaalla <161076919+varaalla@users.noreply.github.com>
2024-06-25 22:04:48 +0300

    send email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# EMAIL_HOST = 'imap.yandex.ru'
+# EMAIL_HOST_USER = 'varaxinaalla@yandex.ru'
+# EMAIL_HOST_PASSWORD = 'iirpzczrqdxvnodk'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
